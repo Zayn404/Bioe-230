9262f10ad96102539e834b54cddefbd55e02d14c
Zainab Almuallim <almualzm@login509-02-r.ibex.kaust.edu.sa>
2024-10-03 15:34:10 +0300

    Question

--- Week3and4/Quesstion4.sh
+++ Week3and4/Quesstion4.sh
@@ -1,14 +1,14 @@
 
 #!/bin/bash
 
-echo > gene_finder_15.txt
+echo > Question4_Outputs.txt
 
 for file in $(find ../ncbi_dataset/data/ -name 'GCA*fna*')
 
 do
 
 echo $file
-echo $file >> gene_finder_15.txt
-python gene_finder1.py -f $file >> gene_finder_15.txt
+echo $file >> Question4_Outputs.txt
+python gene_finder1.py -f $file >> Question4_Outputs.txt
 
 done
